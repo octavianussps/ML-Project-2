d1946a581b7c37aeb34587f525ec4853722d6adf
Valentin Margraf <margrafv@math.hu-berlin.de>
2020-11-17 11:43:11 +0100

    try to train

--- ConvNN.py
+++ ConvNN.py
@@ -4,6 +4,9 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
+
+
+
 class ConvNN(keras.Model):
     
     def __init__(self):
@@ -12,7 +15,7 @@ class ConvNN(keras.Model):
         self.image_size = 400 # images have 400 width, 400 height
         self.num_channels = 3 # RGB channels
         self.alpha = 0.15 # parameter for activation function Leaky ReLu
-        self.dropout_probability = 0.3 # dropout to avoid overfitting
+        self.dropout_probability = 0.8 # dropout to avoid overfitting
         self.num_classes = 2 # groundtruth is black and white image (label 0 and 1)
         self.batch_size = 256
         self.reg_parameter = 1e-6 # parameter for regularization 
@@ -53,7 +56,47 @@ class ConvNN(keras.Model):
         self.model.add(layers.Dense(units=64,activation='relu'))
         self.model.add(layers.Dropout(rate=self.dropout_probability))
         
-        self.model.add(layers.Dense(units=self.num_classes,activation='relu'))
+        self.model.add(layers.Dense(units=self.num_classes,activation='softmax'))
         
         
+        # prints summary of the model
         self.model.summary()
+        
+        # configures the model for training
+        self.model.compile(optimizer="adam", loss=keras.losses.binary_crossentropy,
+                           metrics=['accuracy'])
+        
+        
+        
+        
+    def train(self, imgs, gt_imgs, num_epochs):
+        
+        np.random.seed(0)
+              
+        # This callback reduces the learning rate when the training accuracy does not improve any more
+        lr_callback = keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.5, patience=2,
+                                                        verbose=1, mode='auto', epsilon=0.0001, cooldown=0, min_lr=0)
+
+        # Stops the training process upon convergence
+        stop_callback = keras.callbacks.EarlyStopping(monitor='loss', min_delta=0.0001, patience=2, verbose=1,
+                                                      mode='auto')
+        history = None
+        try:
+            history = self.model.fit(x=imgs,
+                                    y=keras.utils.to_categorical(gt_imgs, num_classes = self.num_classes),
+                                    batch_size=16,
+                                    epochs=num_epochs,
+                                    verbose=1,
+                                     callbacks=[lr_callback, stop_callback], 
+                                     validation_split=0.2,
+                                     shuffle=True,
+                                      workers=1)
+        except KeyboardInterrupt:
+            # Do not throw away the model in case the user stops the training process
+            pass
+
+        print('Training completed')
+        return history
+        
+        
+        
---
+++ helpers.py
@@ -0,0 +1,57 @@
+import matplotlib.image as mpimg
+import numpy as np
+import matplotlib.pyplot as plt
+import os,sys
+from PIL import Image
+from scipy import ndimage, misc
+
+
+def load_image(infilename):
+    data = mpimg.imread(infilename)
+    return data
+
+def img_float_to_uint8(img):
+    rimg = img - np.min(img)
+    rimg = (rimg / np.max(rimg) * 255).round().astype(np.uint8)
+    return rimg
+
+# Concatenate an image and its groundtruth
+def concatenate_images(img, gt_img):
+    nChannels = len(gt_img.shape)
+    w = gt_img.shape[0]
+    h = gt_img.shape[1]
+    if nChannels == 3:
+        cimg = np.concatenate((img, gt_img), axis=1)
+    else:
+        gt_img_3c = np.zeros((w, h, 3), dtype=np.uint8)
+        gt_img8 = img_float_to_uint8(gt_img)          
+        gt_img_3c[:,:,0] = gt_img8
+        gt_img_3c[:,:,1] = gt_img8
+        gt_img_3c[:,:,2] = gt_img8
+        img8 = img_float_to_uint8(img)
+        cimg = np.concatenate((img8, gt_img_3c), axis=1)
+    return cimg
+
+def img_crop(im, w, h):
+    list_patches = []
+    imgwidth = im.shape[0]
+    imgheight = im.shape[1]
+    is_2d = len(im.shape) < 3
+    for i in range(0,imgheight,h):
+        for j in range(0,imgwidth,w):
+            if is_2d:
+                im_patch = im[j:j+w, i:i+h]
+            else:
+                im_patch = im[j:j+w, i:i+h, :]
+            list_patches.append(im_patch)
+    return list_patches
+
+
+def load_images(train_dir, gt_dir, num_images):
+    print("loading images")
+    imgs = np.asarray([load_image(train_dir + "satImage_%.3d" % i + ".png") for i in range(1, num_images + 1)])
+    gt_imgs = np.asarray([load_image(gt_dir + "satImage_%.3d" % i + ".png") for i in range(1, num_images + 1)])
+    print("finished loading images")
+    print("imgs shape : ", end=' ')
+    print(imgs.shape)
+    return imgs, gt_imgs
