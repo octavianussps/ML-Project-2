21f4e4c37c47e5c17dbccf393402d2d6deb9c3bc
octavianussps <octavianussinaga16@gmail.com>
2020-12-02 17:23:13 +0100

    run

--- src/run.py
+++ src/run.py
@@ -15,56 +15,24 @@ TEST_SIZE = 50
 
 
 
-def main(argv):
+def main():
     # We add all test images to an array, used later for generating a submission
     image_filenames_test = [PATH_TEST_DATA + 'test_' + str(i + 1) + '/' + 'test_' + str(i + 1) + '.png' for i
                             in range(TEST_SIZE)]
     image_filenames_predict = [PATH_PREDICTION_DIR + 'pred_' + str(i + 1) + '_unet.png' for i in range(TEST_SIZE)]
     print('Loading Model...')
+    # Run the normal CNN model
+    model = CnnModel()
+    #model.built=True
+    model.load_weights(PATH_WEIGHTS)
+    # Generates the submission
+    #modelType = 2
+    #generate_submission(model, PATH_SUBMISSION, modelType, *image_filenames_test)
+    # IF SMALLER PATCHES CHOSEN, COMPUTE THE MEAN OVER SOME OF THEM
+    # IN ORDER TO SUBMIT ON AICROWD
+    modelType = 2
+    generate_submission(model, PATH_SUBMISSION, modelType, *image_filenames_test)
+            
     
-
-    
-    if len(argv) != 2:
-        print(len(argv))
-        raise Exception('Please, pass only one argument to the script')
-    else:
-        if argv[1] == '-unet':
-
-            # Run the UNET model
-            model_unet = keras.models.load_model(PATH_UNET)
-            gen_image_predictions_unet(model_unet, PATH_PREDICTION_DIR, *image_filenames_test)
-
-            # Generates the submission
-            modelType = 1
-            generate_submission(model_unet, PATH_SUBMISSION, modelType, *image_filenames_predict)
-
-        elif argv[1] == '-normal':
-            # Run the normal CNN model
-            model = CnnModel()
-            #model.built=True
-            model.load_weights(PATH_WEIGHTS)
-            # Generates the submission
-            #modelType = 2
-            #generate_submission(model, PATH_SUBMISSION, modelType, *image_filenames_test)
-            
-            # IF SMALLER PATCHES CHOSEN, COMPUTE THE MEAN OVER SOME OF THEM
-            # IN ORDER TO SUBMIT ON AICROWD
-            modelType = 2
-            generate_submission(model, PATH_SUBMISSION, modelType, *image_filenames_test)
-            
-                
-        elif argv[1] == '-reg':
-            #run the regression model
-            model = cnnRegression()
-            model.load_weights(PATH_REG)
-            ### fix!!!
-            
-            generate_submission(model, PATH_SUBMISSION, False, *image_filenames_test)
-
-        
-        else:
-            raise Exception('Please pass only "unet" or "normal" as argument to the script')
-
-
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
