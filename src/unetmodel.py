791f6b8310072e4a670a75e39cb5fb66195a50bb
octavianussps <octavianussinaga16@gmail.com>
2020-12-12 18:14:10 +0100

    .

--- src/unetmodel.py
+++ src/unetmodel.py
@@ -9,43 +9,60 @@ NUM_CHANNELS =3
      
 def double_conv(data, out_channel):
 """
+Generate 2 convolutional layer for the model
+Inputs:
+   data = embedded image (tensor)
+   out_channel = the size of the output channel
 
+Outputs:
+   conv = convolution layers for the model
 """
     # convolution block, 2 conv layers with relu each
     conv = layers.Conv2D(filters=out_channel, kernel_size=3, padding='same')(data)
-    #conv = layers.ReLU()(conv)
     conv = layers.LeakyReLU(alpha=0.3)(conv)
     conv = layers.Conv2D(filters=out_channel, kernel_size=3, padding='same')(conv)
-    #conv = layers.ReLU()(conv)
     conv = layers.LeakyReLU(alpha=0.3)(conv)
     return conv
     
     
-def max_pool(data):        
+def max_pool(data):
+"""
+Generate maximum pooling layer
+Inputs:
+   data = embedded image (tensor)
+
+Outputs:
+   max_pooled = pooling layer
+"""       
     max_pooled = layers.MaxPooling2D(pool_size=(2, 2), strides=(2,2), padding="same")(data)
     max_pooled = layers.Dropout(0.5)(max_pooled)
     return max_pooled
 
 
-def crop_img(tensor, target_tensor):
-    # helper function if one chooses padding = 'valid' in the max pool layers
-    target_size = target_tensor.get_shape()[2]
-    tensor_size = tensor.get_shape()[2]
-    delta = tensor_size-target_size
-    delta = delta // 2
-    #print("tensor",tensor.get_shape())
-    #print("target",target_tensor.get_shape())
-    tensor = tensor[:,delta:tensor_size-delta, delta:tensor_size-delta,:]
-    #print(tensor)
-    return tensor
-
 def up_conv(data, out_channels):
+"""
+Generate up convolutional layer for the model
+Inputs:
+   data = embedded image (tensor)
+   out_channel = the size of the output channel
+
+Outputs:
+   upconv = up convolution layers for the model
+"""
     upconv = layers.Conv2DTranspose(filters=out_channels, kernel_size=2, strides=(2,2))(data)
     upconv = layers.Dropout(0.5)(upconv)
     return upconv    
         
 def unet(data, num_filters):
-    
+"""
+Generate unet layer
+Inputs:
+   data = embedded image (tensor)
+   num_filters = number of filters
+
+Outputs:
+   out = unet layer
+"""
     #encoder
     x1 = double_conv(data, num_filters)
     x2 = max_pool(x1)
@@ -87,6 +104,15 @@ def unet(data, num_filters):
     
 
 def unet_model(img_size, num_filters):
+"""
+Generate unet model
+Inputs:
+   img_size  = image size
+   num_filters = number of filters
+
+Outputs:
+   model = unet model
+"""
     #instantiate and compile the model
     
     inputs = layers.Input((img_size, img_size, NUM_CHANNELS))
@@ -105,9 +131,19 @@ def unet_model(img_size, num_filters):
 
 
 def train(model, x_train, y_train, batch_size, num_epochs):
-    """Optimize the model and return the model with the training F1-Score"""
+"""
+Optimize the model and return the model with the training F1-Score
+Inputs:
+   model = unet model
+   x_train = training images
+   y_train = ground truth images
+   batch_size = batch size
+   num_epochs = number of epoch
+
+Outputs:
+   model = unet model
+"""
     try:
-       #checkpoint = ModelCheckpoint("../models/unetBestWeights.h5",monitor='loss',verbose =1,save_best_only = True,mode = 'auto',period = 5)
         checkpoint = ModelCheckpoint("../models/unetLReLU.h5",monitor='loss',verbose =1,save_best_only = True,mode = 'auto',period = 5)
         history = model.fit(x_train, y_train, batch_size=batch_size, epochs=num_epochs, callbacks=[checkpoint], validation_split=0.1)
     except KeyboardInterrupt:
